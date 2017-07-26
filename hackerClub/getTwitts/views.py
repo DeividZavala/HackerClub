cb74f78b7c182f82ed707d9ae7e682e57707051e
Efrain Rodriguez <braincaus@gmail.com>
2017-07-25 22:11:12 -0500

    Modificacion en la creacion del archivo, para mostrar el texto, la ubicacion desde donde se envio, el numero de hashtags usados y el numero de retweets que tuve

--- hackerClub/getTwitts/views.py
+++ hackerClub/getTwitts/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 
 
 
-def index():
+def uff():
     import twitter
 
     api = twitter.Api(consumer_key='CWzj0wObKkKB2MB7px5Kd7Oaq',
@@ -13,18 +13,23 @@ def index():
                       access_token_key='48614527-N0V8ArqBdMWWnFpWeBG7tsp3aUk22ZxxkAqKxclUq',
                       access_token_secret='nuOolGsVC9vlcvwdWw4PbAA8OnmH6NKopO3z1GnuMd8rI')
 
-    results = api.GetSearch(
-        raw_query="q=twitter%20&result_type=recent&since=2014-07-19&count=100")
-
-    return "Hola Mundo"
+    return api.GetUserTimeline(screen_name='braincaus', count=200)
 
 def get_file(request):
     # Create the HttpResponse object with the appropriate CSV header.
     response = HttpResponse(content_type='text/csv')
     response['Content-Disposition'] = 'attachment; filename="somefilename.csv"'
 
+    data = uff()
+
     writer = csv.writer(response)
-    writer.writerow(['First row', 'Foo', 'Bar', 'Baz'])
-    writer.writerow(['Second row', 'A', 'B', 'C', '"Testing"', "Here's a quote"])
+    writer.writerow(["Texto", "Ubicacion", "hashtags", "retweets"])
+
+    for x in data:
+        try:
+            print(x)
+            writer.writerow([x.text, x.location, len(x.hashtags), x.retweet_count])
+        except Exception as e:
+            print ("Error de caracter")
 
     return response
