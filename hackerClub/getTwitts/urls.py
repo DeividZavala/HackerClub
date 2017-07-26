8b94861c6465cb0ef682f9ebc2a742cdd4d43cd9
Efrain Rodriguez <braincaus@gmail.com>
2017-07-25 21:08:55 -0500

    Se agrego funcion basica para crear archivo csv

--- hackerClub/getTwitts/urls.py
+++ hackerClub/getTwitts/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.getUser.as_view(), name="user")
+	#url(r'^$', views.getUser.as_view(), name="user"),
+	url(r'^archivo', views.get_file, name="archivo"),
 ]
